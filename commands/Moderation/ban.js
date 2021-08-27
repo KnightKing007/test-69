62cf9022576da62b77f97f072eb54774320616e3
Knight King <57825656+KnightKing007@users.noreply.github.com>
2021-08-27 13:15:50 +0530

    sussy baka

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -2,14 +2,14 @@ const discord = require('discord.js')
 const { Description } = require('../Util/help')
 
 module.exports = {
-    name: "Kick",
-    Description: "Kicks a member",
+    name: "Ban",
+    Description: "Bans a member",
     async run (client, messages, args,){
         const member = message.mentions.users.first();
         if(member){
             const memberTarger = message.guild.members.cache.get(member.id);
-            memberTarger.kick();
-            message.channel.send("${<@member.id>} Has been Kicked");
+            memberTarger.Ban();
+            message.channel.send("${<@member.id>} Has been Ban");
         }else{
             message.channel.send('Could not kick the specified user pog¯\_(ツ)_/¯ ')
         }
