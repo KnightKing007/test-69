b1ad649cf6d2467c85b00d0a75f8ccef7db65a98
Knight King <57825656+KnightKing007@users.noreply.github.com>
2021-11-05 12:49:59 +0530

    bugged

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -1,21 +1,14 @@
 const fs = require('fs');
-// const command = require(`./commands/${folder}/${file}`);
 
 module.exports = (client, Discord ) =>{
 
-const commandFolders = fs.readdirSync('../commands');
-console.log("Folders are :" , commandFolders);
+const commandFolders = fs.readdirSync('./commands');
 
 for (const folder of commandFolders) {
-    console.log("The folder is", folder);
-    const commandFiles = fs.readdirSync(`../commands/${folder}`).filter(file => file.endsWith('.js'));
-    console.log("The command files are", commandFiles);
+    const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
     for (const file of commandFiles) {
-        console.log("The file is", file);
         const command = require(`../commands/${folder}/${file}`);
-        console.log("Command is :", command);
         const {name} = command;
-        console.log("Name of command is :", name);
         client.commands.set(name, command);
     }
   }
