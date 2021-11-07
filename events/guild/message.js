487d3e49737534c04dd09656f4ef01311eb876dc
Knight King <57825656+KnightKing007@users.noreply.github.com>
2021-11-07 12:56:01 +0530

    message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,11 +1,32 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log(client);
+
 module.exports = (Discord, client, message) => {
-    const prefix = '+'
-    if(message.content.startsWith(prefix) || message.author.bot) return;
+//     const prefix = '+'
+//     if(message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const cmd = args.shift().toLowerCase();
+//     const args = message.content.slice(prefix.length).split(/ +/);
+//     const cmd = args.shift().toLowerCase();
 
-    const command = client.commands.get(cmd);
+//     const command = client.commands.get(cmd);
 
-    if(command) command.execute(client, message, args, Discord)
+//     if(command) command.execute(client, message, args, Discord)
+// }
+
+client.on('message', async message => {
+	let prefix = '+'
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const commandFolders = args.shift().toLowerCase();
+	if (!client.commands.has(commandFolders)) return;
+
+try {
+	client.commands.get(commandsFolders).run(client, message, args,);
+} catch (error) {
+	console.error(error);
+	message.reply('Error 69 could not run that command :thinking:');
+}
+})
 }
